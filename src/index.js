e2365e02bea3cffcd6411ab3b5761f7fcf65ced5
Kiwiproperty <peter.g0103@gmail.com>
2026-04-26 16:00:26 +0800

    Move CI flag after imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-﻿process.env.CI = false;
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';  // ← MUST HAVE THIS
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+process.env.CI = false;  // ← Move this AFTER imports
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>  {/* ← WRAP HERE */}
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
